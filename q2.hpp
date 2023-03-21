2458bae13ff99c2f8a5a37586ba18fe29d6842ea
Ugalde <jocelin.uga513@gmail.com>
2023-03-21 15:17:50 -0700

    1

--- q2.hpp
+++ q2.hpp
@@ -3,5 +3,36 @@
 using namespace std;
 
 // ******************************
-// Implement all your functions here
+void getInput(int &, int &, int &);
+int findMin(int &, int &, int &);
+void printResult(int &, int &, int &, int &);
+
+void getInput(int &n1, int &n2, int &n3)
+{
+    cin >> n1 >> n2 >> n3;
+}
+
+int findMin(int &n1, int &n2, int &n3)
+{
+    int min;
+    if (n1 < n2 && n1 < n3)
+    {
+        min = n1;
+    }
+    else if (n2 < n1 && n2 < n3)
+    {
+        min = n2;
+    }
+    else
+    {
+        min = n3;
+    }
+    return min;
+}
+
+void printResult(int &n1, int &n2, int &n3, int &min)
+{
+    cout
+        << n1 << " " << n2 << " " << n3 << " " << min;
+}
 // ******************************
